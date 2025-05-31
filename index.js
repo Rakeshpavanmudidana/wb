30b34e5f0db4eaf6033eb1c27b2b11231c964731
Rakeshpavanmudidana <137647605+Rakeshpavanmudidana@users.noreply.github.com>
2025-06-01 00:07:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
 
-window.addEventListener("DOMContentLoaded", function () {
-  const dobInput = document.getElementById("dob");
-  const today = new Date();
-
-  const maxDate = new Date(today.getFullYear() - 18, today.getMonth(), today.getDate());
-  const minDate = new Date(today.getFullYear() - 55, today.getMonth(), today.getDate());
-
-  function formatDate(date) {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
-  }
-
-  dobInput.max = formatDate(maxDate);
-  dobInput.min = formatDate(minDate);
-});
-
-
 const dobInput = document.getElementById("dob");
 dobInput.addEventListener("input", function () {
   const value = new Date(this.value);
