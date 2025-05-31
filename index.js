ffc50f85a58742b7c3448981afeb9ad341776651
Rakeshpavanmudidana <137647605+Rakeshpavanmudidana@users.noreply.github.com>
2025-06-01 00:50:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ window.addEventListener("DOMContentLoaded", function () {
   const maxDate = new Date(today.getFullYear() - 18, today.getMonth(), today.getDate());
   const minDate = new Date(today.getFullYear() - 55, today.getMonth(), today.getDate());
 
-  function formatDate(date) {
+  function formatDate(date) { 
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
